502b1192250cdfd4a155e02fff0d7539a215369d
jamshid <jamshidqochqorov84@gmail.com>
2022-12-29 09:42:57 +0500

    added seeder Teacher  php artisan db:seed --class=TeacherSeeder

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
+
 
 class RegisterController extends Controller
 {
---
+++ database/seeders/TeacherSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Seeder;
+
+class TeacherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = Role::create([
+            'name' => 'Teacher',
+            'title' => 'Teacher',
+            'guard_name' => 'web'
+        ]);
+
+        $permissions = Permission::insert([
+            [
+                'name'=>'teacher.show',
+                'title'=>"Ustozlarni ko'rish",
+                'guard_name'=>'web'
+            ],
+            [
+                'name'=>'teacher.edit',
+                'title'=>"Ustozni tahrirlash",
+                'guard_name'=>'web'
+            ],
+            [
+                'name'=>'teacher.add',
+                'title'=>"Ustoz qo'shish",
+                'guard_name'=>'web'
+            ],
+            [
+                'name'=>'teacher.delete',
+                'title'=>"Ustozni o'chirish",
+                'guard_name'=>'web'
+            ]
+        ]);
+
+    }
+}
